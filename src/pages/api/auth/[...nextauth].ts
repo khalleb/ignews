214c957745be64b895b82e780ecf97f887c60615
Khalleb <khallebw@gmail.com>
2021-03-23 21:04:21 -0300

    FaunaDB

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,8 @@
+import { query as q } from 'faunadb'
 import NextAuth from 'next-auth'
 import Providers from 'next-auth/providers'
+
+import { fauna } from '../../../services/fauna'
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -10,4 +13,23 @@ export default NextAuth({
       scope: 'read:user'
     }),
   ],
+  jwt: {
+    signingKey: process.env.SIGNING_KEY
+  },
+  callbacks: {
+    async signIn(user, account, profile) {
+      const { email } = user;
+      try {
+        await fauna.query(
+          q.Create(
+            q.Collection('users'),
+            { data: { email } }
+          )
+        )
+        return true;
+      } catch {
+        return false
+      }
+    },
+  }
 })
